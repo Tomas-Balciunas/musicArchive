d5fdba03ef8282ad1ce6b0b04b325d00af2f518b
Tomas-Balciunas <tomas.balciunas@stud.kitm.lt>
2024-07-01 18:50:37 +0300

    deployment set up

--- client/e2e/home.test.ts
+++ client/e2e/home.test.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test'
 test('visitor can see the homepage heading', async ({ page }) => {
   await page.goto('/')
 
-  const heading = page.getByRole('heading', { name: 'Music archive', exact: true })
+  const heading = page.getByRole('heading', { name: 'Bands:', exact: true })
 
   await expect(heading).toBeVisible()
 })
